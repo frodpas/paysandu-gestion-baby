a133ba769052186701cda0929f8411746ce4938d
frodpas <frodrigu@adinet.com.uy>
2026-04-17 11:51:54 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1452,6 +1452,7 @@ function AdminScreen({ user, onLogout }) {
                     </div>
                   );
                 })}
+                </div>{/* fin tabla pendientes */}
               </>
             )}
             {pendientes.length===0&&(
