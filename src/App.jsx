73f05ddfed47f000eb652a79fce9861ba7b442ae
frodpas <frodrigu@adinet.com.uy>
2026-05-20 08:25:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";// v2
 
 /* ══ CONFIG ═══════════════════════════════════════════════════════════ */
 const SB_URL = "https://ipgvsrmlgavsmickwrst.supabase.co";
